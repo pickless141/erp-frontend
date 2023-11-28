7b40bc91536ba5929058180237721b56f11da966
Ivan <125994256+pickless141@users.noreply.github.com>
2023-11-28 18:22:24 -0300

    add: paginacion y searchbar en insumos

--- src/views/insumos/Insumos.jsx
+++ src/views/insumos/Insumos.jsx
@@ -1,46 +1,71 @@
-import Layout from '../../components/Layout';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { FaEdit } from "react-icons/fa";
+import Layout from '../../components/Layout';
 import EditarInsumo from './EditarInsumo';
+import { FaEdit } from 'react-icons/fa';
 
 const Insumos = () => {
   const navigate = useNavigate();
-  const [insumos, setInsumos] = useState([]);
-  const [editarInsumo, setEditarInsumo] = useState(null);
+  const [insumos, setInsumos] = useState({ docs: [], totalDocs: 0, limit: 10 });
+  const [insumoEditarId, setInsumoEditarId] = useState(null);
 
-  useEffect(() => {
-    const apiUrl = import.meta.env.VITE_API_SERVER;
-    const token = localStorage.getItem('token');
-    fetch(`${apiUrl}/insumos/`, {
-      method: 'GET',
-      headers: {
-        'x-auth-token': token,
-      },
-    })
-    .then((response) => {
-      if (response.ok){
-        return response.json();
-      } else {
-        throw new Error('Error al buscar insumos');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const fetchInsumos = useCallback(async (token, page, search) => {
+    try {
+      const apiUrl = import.meta.env.VITE_API_SERVER;
+      const response = await fetch(`${apiUrl}/insumos/?page=${page}&search=${search}`, {
+        method: 'GET',
+        headers: {
+          'x-auth-token': token,
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error al obtener los datos. Código de estado: ${response.status}`);
       }
-    })
-    .then((data) => {
-      setInsumos(data);
-    })
-    .catch((error) => {
-      console.log('Error al obtener productos:', error);
-    });
+  
+      const data = await response.json();
+  
+      if (data.docs && data.docs.length > 0 && data.docs[0]._id) {
+        setInsumos({ docs: data.docs, totalDocs: data.totalDocs, limit: data.limit });
+      } else {
+        console.error('La estructura de la respuesta no es la esperada:', data);
+      }
+    } catch (error) {
+      console.error('Error al procesar la respuesta del servidor:', error);
+    }
   }, []);
 
-  const handleEditar = (insumoId) => {
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      fetchInsumos(token, currentPage, searchTerm);
+    } else {
+      console.error('Token no disponible');
+    }
+  }, [currentPage, searchTerm, fetchInsumos]);
+
+  const handleEditarInsumoClick = (insumoId) => {
     if (insumoId) {
-      setEditarInsumo(insumoId);
+      setInsumoEditarId(insumoId);
       navigate(`/editarinsumo/${insumoId}`);
     } else {
       console.error('insumoId es undefined');
     }
   };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+    setCurrentPage(1);
+  };
+
+  const pageCount = Math.ceil(insumos.totalDocs / insumos.limit);
 
   return (
     <Layout>
@@ -51,18 +76,28 @@ const Insumos = () => {
       >
         Nuevo Insumo
       </Link>
+
+      <input
+        type="text"
+        placeholder="Buscar por nombre de insumo"
+        value={searchTerm}
+        onChange={(e) => handleSearch(e.target.value)}
+        className="block w-full py-2 px-3 border rounded shadow-sm mb-4"
+      />
+
       <div className="overflow-x-scroll">
-        <table className="table-auto shadow-md w-full">
-          <thead className="bg-gray-800 text-white">
-            <tr>
-              <th className="w-1/4 py-2 px-4">Producto</th>
-              <th className="w-1/4 py-2 px-4">Peso</th>
-              <th className="w-1/4 py-2 px-4">Descripcion</th>
-              <th className="w-1/4 py-2 px-4">Editar</th>
+        <table className="table-auto shadow-md mt-10 w-full lg:w-auto">
+          <thead className="bg-gray-800">
+            <tr className="text-white">
+              <th className="w-1/4 py-2">Producto</th>
+              <th className="w-1/4 py-2">Peso</th>
+              <th className="w-1/4 py-2">Descripción</th>
+              <th className="w-1/4 py-2">Editar</th>
             </tr>
           </thead>
+
           <tbody className="bg-white">
-            {insumos.map((insumo, index) => (
+            {insumos.docs.map((insumo, index) => (
               <tr key={index}>
                 <td className="border px-4 py-2">{insumo.producto}</td>
                 <td className="border px-4 py-2">{insumo.peso}</td>
@@ -70,12 +105,11 @@ const Insumos = () => {
                 <td className="border px-4 py-2">
                   <button
                     onClick={() => {
-                      handleEditar(insumo._id);
+                      handleEditarInsumoClick(insumo._id);
                     }}
                     className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded"
                   >
-                    <FaEdit color="white" 
-                    />
+                    <FaEdit color="white" />
                   </button>
                 </td>
               </tr>
@@ -83,7 +117,24 @@ const Insumos = () => {
           </tbody>
         </table>
       </div>
-      {editarInsumo && <EditarInsumo insumoId={editarInsumo}/>}
+
+      {pageCount > 1 && (
+        <div className="flex justify-center mt-4">
+          {[...Array(pageCount)].map((_, index) => (
+            <button
+              key={index}
+              onClick={() => handlePageChange(index + 1)}
+              className={`mx-1 px-3 py-1 rounded ${
+                currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-300'
+              }`}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
+      )}
+
+      {insumoEditarId && <EditarInsumo insumoId={insumoEditarId} />}
     </Layout>
   );
 };
